84f0c9c097400176a16969545370e6882bc8d2b8
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-07 16:40:46 +0900

    refactor binary_search

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -14,37 +14,17 @@ pub trait BinarySearch<T> {
 #[snippet(prefix = "use std::cmp::Ordering;")]
 impl<T: Ord> BinarySearch<T> for [T] {
     fn lower_bound(&self, x: &T) -> usize {
-        let mut low = 0;
-        let mut high = self.len();
-        while low != high {
-            let mid = (low + high) / 2;
-            match self[mid].cmp(x) {
-                Ordering::Less => {
-                    low = mid + 1;
-                }
-                Ordering::Equal | Ordering::Greater => {
-                    high = mid;
-                }
-            }
-        }
-        low
+        self.partition_point(|y| match y.cmp(x) {
+            Ordering::Less => true,
+            Ordering::Equal | Ordering::Greater => false,
+        })
     }
 
     fn upper_bound(&self, x: &T) -> usize {
-        let mut low = 0;
-        let mut high = self.len();
-        while low != high {
-            let mid = (low + high) / 2;
-            match self[mid].cmp(x) {
-                Ordering::Less | Ordering::Equal => {
-                    low = mid + 1;
-                }
-                Ordering::Greater => {
-                    high = mid;
-                }
-            }
-        }
-        low
+        self.partition_point(|y| match y.cmp(x) {
+            Ordering::Less | Ordering::Equal => true,
+            Ordering::Greater => false,
+        })
     }
 
     fn partition_point<F>(&self, mut f: F) -> usize

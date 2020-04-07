ef05cc9b34d87293c92566791a8d4eda7b0c8d9d
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-07 16:21:34 +0900

    add binary_search

---
+++ src/binary_search.rs
@@ -0,0 +1,66 @@
+use cargo_snippet::snippet;
+use std::cmp::Ordering;
+
+#[snippet("BinarySearch")]
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+    fn partition_point<F>(&self, f: F) -> usize
+    where
+        F: FnMut(&T) -> bool;
+}
+
+#[snippet("BinarySearch")]
+#[snippet(prefix = "use std::cmp::Ordering;")]
+impl<T: Ord> BinarySearch<T> for Vec<T> {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn partition_point<F>(&self, mut f: F) -> usize
+    where
+        F: FnMut(&T) -> bool,
+    {
+        let mut low = 0;
+        let mut high = self.len();
+        while low != high {
+            let mid = (low + high) / 2;
+            if f(&self[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        low
+    }
+}

762e5f27b77e5c396cb12a7793c5d3d52b11846e
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-27 14:22:28 +0900

    Refine abc164_e

--- examples/abc164_e.rs
+++ examples/abc164_e.rs
@@ -1,7 +1,6 @@
 use proconio::{fastout, input};
 
 use proconio::marker::Usize1;
-use std::cmp::max;
 use std::cmp::min;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
@@ -9,46 +8,42 @@ use std::collections::BinaryHeap;
 #[fastout]
 fn main() {
     input! {
-        n: usize, m: usize, s: i64,
+        n: usize, m: usize, s: usize,
     }
     let mut g = vec![vec![]; n];
-    for i in 0..m {
-        input! { u: Usize1, v: Usize1, a: i64, b: i64 }
+    for _ in 0..m {
+        input! { u: Usize1, v: Usize1, a: usize, b: u64 }
         g[u].push((v, a, b));
         g[v].push((u, a, b));
     }
     let mut h = vec![(0, 0); n];
     for i in 0..n {
-        input! { c: i64, d: i64 }
+        input! { c: usize, d: u64 }
         h[i] = (c, d);
     }
     let mut q = BinaryHeap::new();
     q.push(Reverse((0, min(s, 2500), 0)));
-    let mut dp = vec![vec![std::i64::MAX; 2501]; n];
-    dp[0][min(s, 2500) as usize] = 0;
-    while let Some(Reverse((t, s, i))) = q.pop() {
+    let mut dp = vec![vec![std::u64::MAX; 2501]; n];
+    dp[0][min(s, 2500)] = 0;
+    while let Some(Reverse((_, s, i))) = q.pop() {
         let (c, d) = h[i];
         if s + c < 2501 {
-            let s0 = s as usize;
-            let s1 = (s + c) as usize;
-            if dp[i][s1] > dp[i][s0] + d {
-                dp[i][s1] = dp[i][s0] + d;
-                q.push(Reverse((dp[i][s1], s1 as i64, i)));
+            if dp[i][s + c] > dp[i][s] + d {
+                dp[i][s + c] = dp[i][s] + d;
+                q.push(Reverse((dp[i][s + c], s + c, i)));
             }
         }
         for &(j, a, b) in g[i].iter() {
-            if s - a >= 0 {
-                let s0 = s as usize;
-                let s1 = (s - a) as usize;
-                if dp[j][s1] > dp[i][s0] + b {
-                    dp[j][s1] = dp[i][s0] + b;
-                    q.push(Reverse((dp[j][s1], s1 as i64, j)));
+            if s >= a {
+                if dp[j][s - a] > dp[i][s] + b {
+                    dp[j][s - a] = dp[i][s] + b;
+                    q.push(Reverse((dp[j][s - a], s - a, j)));
                 }
             }
         }
     }
     for i in 1..n {
-        let ans = dp[i].iter().fold(std::i64::MAX, |x, y| min(x, *y));
+        let ans = dp[i].iter().min().unwrap();
         println!("{}", ans);
     }
 }

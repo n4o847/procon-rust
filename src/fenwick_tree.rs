ab92a7bf7803ead615d328e0d5f413b804179a62
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-03-15 20:47:38 +0900

    Modularize fenwick_tree

--- src/fenwick_tree.rs
+++ src/fenwick_tree.rs
@@ -1,50 +1,56 @@
-struct FenwickTree<T> {
-  data: Vec<T>,
-}
+use cargo_snippet::snippet;
 
-impl<T> FenwickTree<T>
-where
-  T: Copy + std::ops::AddAssign + std::ops::Sub<Output = T> + num::traits::Zero,
-{
-  #[allow(dead_code)]
-  fn new(n: usize) -> Self {
-    Self {
-      data: vec![T::zero(); n],
-    }
+#[snippet("fenwick_tree")]
+#[snippet(prefix = "use fenwick_tree::*;")]
+mod fenwick_tree {
+  pub struct FenwickTree<T> {
+    data: Vec<T>,
   }
 
-  #[allow(dead_code)]
-  fn add(&mut self, mut i: usize, x: T) {
-    i += 1;
-    while i <= self.data.len() {
-      self.data[i - 1] += x;
-      i += i & i.wrapping_neg();
+  impl<T> FenwickTree<T>
+  where
+    T: Copy + std::ops::AddAssign + std::ops::Sub<Output = T> + num::traits::Zero,
+  {
+    #[allow(dead_code)]
+    pub fn new(n: usize) -> Self {
+      Self {
+        data: vec![T::zero(); n],
+      }
     }
-  }
 
-  #[allow(dead_code)]
-  fn cumsum(&self, mut i: usize) -> T {
-    let mut s = T::zero();
-    while i > 0 {
-      s += self.data[i - 1];
-      i -= i & i.wrapping_neg();
+    #[allow(dead_code)]
+    pub fn add(&mut self, mut i: usize, x: T) {
+      i += 1;
+      while i <= self.data.len() {
+        self.data[i - 1] += x;
+        i += i & i.wrapping_neg();
+      }
     }
-    s
-  }
 
-  #[allow(dead_code)]
-  fn sum(&self, index: impl std::ops::RangeBounds<usize>) -> T {
-    use std::ops::Bound::*;
-    let l = match index.start_bound() {
-      Included(&i) => i,
-      Excluded(&i) => i + 1,
-      Unbounded => 0,
-    };
-    let r = match index.end_bound() {
-      Included(&i) => i + 1,
-      Excluded(&i) => i,
-      Unbounded => self.data.len() - 1,
-    };
-    self.cumsum(r) - self.cumsum(l)
+    #[allow(dead_code)]
+    pub fn cumsum(&self, mut i: usize) -> T {
+      let mut s = T::zero();
+      while i > 0 {
+        s += self.data[i - 1];
+        i -= i & i.wrapping_neg();
+      }
+      s
+    }
+
+    #[allow(dead_code)]
+    pub fn sum(&self, index: impl std::ops::RangeBounds<usize>) -> T {
+      use std::ops::Bound::*;
+      let l = match index.start_bound() {
+        Included(&i) => i,
+        Excluded(&i) => i + 1,
+        Unbounded => 0,
+      };
+      let r = match index.end_bound() {
+        Included(&i) => i + 1,
+        Excluded(&i) => i,
+        Unbounded => self.data.len() - 1,
+      };
+      self.cumsum(r) - self.cumsum(l)
+    }
   }
 }

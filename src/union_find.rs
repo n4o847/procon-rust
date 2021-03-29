e599210c04391f001fd678a64d56df960a46bd9d
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-03-29 19:49:00 +0900

    Update union_find

--- src/union_find.rs
+++ src/union_find.rs
@@ -1,44 +1,75 @@
-struct UnionFind {
-  parent: Vec<usize>,
-  size: Vec<usize>,
-}
+use cargo_snippet::snippet;
 
-impl UnionFind {
-  #[allow(dead_code)]
-  fn new(n: usize) -> Self {
-    Self {
-      parent: (0..n).collect(),
-      size: vec![1; n],
-    }
+#[snippet("use union_find")]
+#[snippet(prefix = "use union_find::*;")]
+mod union_find {
+  pub struct UnionFind {
+    parent: Vec<usize>,
+    size: Vec<usize>,
   }
 
-  #[allow(dead_code)]
-  fn find(&mut self, x: usize) -> usize {
-    if self.parent[x] == x {
-      x
-    } else {
-      let p = self.find(self.parent[x]);
-      self.parent[x] = p;
-      p
+  impl UnionFind {
+    #[allow(dead_code)]
+    pub fn new(n: usize) -> Self {
+      Self {
+        parent: (0..n).collect(),
+        size: vec![1; n],
+      }
     }
-  }
 
-  #[allow(dead_code)]
-  fn unite(&mut self, x: usize, y: usize) {
-    let mut x = self.find(x);
-    let mut y = self.find(y);
-    if x == y {
-      return;
+    #[allow(dead_code)]
+    pub fn find(&mut self, x: usize) -> usize {
+      if self.parent[x] == x {
+        x
+      } else {
+        let p = self.find(self.parent[x]);
+        self.parent[x] = p;
+        p
+      }
     }
-    if self.size[x] < self.size[y] {
-      std::mem::swap(&mut x, &mut y);
-    }
-    self.parent[y] = x;
-    self.size[x] += self.size[y];
-  }
 
-  #[allow(dead_code)]
-  fn same(&mut self, x: usize, y: usize) -> bool {
-    self.find(x) == self.find(y)
+    #[allow(dead_code)]
+    pub fn unite(&mut self, x: usize, y: usize) {
+      let mut x = self.find(x);
+      let mut y = self.find(y);
+      if x == y {
+        return;
+      }
+      if self.size[x] < self.size[y] {
+        std::mem::swap(&mut x, &mut y);
+      }
+      self.parent[y] = x;
+      self.size[x] += self.size[y];
+    }
+
+    #[allow(dead_code)]
+    pub fn same(&mut self, x: usize, y: usize) -> bool {
+      self.find(x) == self.find(y)
+    }
+
+    #[allow(dead_code)]
+    pub fn size(&mut self, x: usize) -> usize {
+      let x = self.find(x);
+      self.size[x]
+    }
+
+    #[allow(dead_code)]
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+      let n = self.parent.len();
+      let mut leader_buf = vec![0; n];
+      let mut group_size = vec![0; n];
+      for i in 0..n {
+        leader_buf[i] = self.find(i);
+        group_size[leader_buf[i]] += 1;
+      }
+      let mut result = vec![Vec::new(); n];
+      for i in 0..n {
+        result[i].reserve(group_size[i]);
+      }
+      for i in 0..n {
+        result[leader_buf[i]].push(i);
+      }
+      result.into_iter().filter(|v| !v.is_empty()).collect()
+    }
   }
 }

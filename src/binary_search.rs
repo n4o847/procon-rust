2b74524936fc85ac95067069f01ae86f6d0f0b0d
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-03-31 18:08:37 +0900

    Update binary_search

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,54 +1,68 @@
 use cargo_snippet::snippet;
-use std::cmp::Ordering;
 
-#[snippet("BinarySearch")]
-pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: &T) -> usize;
-    fn upper_bound(&self, x: &T) -> usize;
-    fn partition_point<F>(&self, f: F) -> usize
-    where
-        F: FnMut(&T) -> bool;
-}
+#[snippet("use binary_search")]
+#[snippet(prefix = "use binary_search::*;")]
+mod binary_search {
+    use std::cmp::Ordering::*;
+    use std::ops::Range;
 
-#[snippet("BinarySearch")]
-#[snippet(prefix = "use std::cmp::Ordering;")]
-impl<T: Ord> BinarySearch<T> for [T] {
-    fn lower_bound(&self, x: &T) -> usize {
-        self.partition_point(|y| match y.cmp(x) {
-            Ordering::Less => true,
-            Ordering::Equal | Ordering::Greater => false,
-        })
+    pub trait BinarySearch<T> {
+        fn lower_bound(&self, x: &T) -> usize;
+        fn upper_bound(&self, x: &T) -> usize;
+        fn equal_range(&self, x: &T) -> Range<usize>;
+        fn partition_point<F>(&self, f: F) -> usize
+        where
+            F: FnMut(&T) -> bool;
     }
 
-    fn upper_bound(&self, x: &T) -> usize {
-        self.partition_point(|y| match y.cmp(x) {
-            Ordering::Less | Ordering::Equal => true,
-            Ordering::Greater => false,
-        })
-    }
-
-    fn partition_point<F>(&self, mut f: F) -> usize
-    where
-        F: FnMut(&T) -> bool,
-    {
-        let mut low = 0;
-        let mut high = self.len();
-        while low != high {
-            let mid = (low + high) / 2;
-            if f(&self[mid]) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+    impl<T: Ord> BinarySearch<T> for [T] {
+        fn lower_bound(&self, x: &T) -> usize {
+            self.partition_point(|y| match y.cmp(x) {
+                Less => true,
+                Equal | Greater => false,
+            })
         }
-        low
+
+        fn upper_bound(&self, x: &T) -> usize {
+            self.partition_point(|y| match y.cmp(x) {
+                Less | Equal => true,
+                Greater => false,
+            })
+        }
+
+        fn equal_range(&self, x: &T) -> Range<usize> {
+            self.lower_bound(x)..self.upper_bound(x)
+        }
+
+        fn partition_point<F>(&self, mut f: F) -> usize
+        where
+            F: FnMut(&T) -> bool,
+        {
+            let mut low = 0;
+            let mut high = self.len();
+            while low != high {
+                let mid = (low + high) / 2;
+                if f(&self[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            low
+        }
     }
 }
 
-#[test]
-fn test_binary_search() {
-    let v = vec![1, 1, 2, 3, 5, 8, 13];
-    assert_eq!(v.lower_bound(&8), 5);
-    assert_eq!(v.upper_bound(&8), 6);
-    assert_eq!(v.partition_point(|x| *x < 8), 5);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use binary_search::*;
+
+    #[test]
+    fn test_binary_search() {
+        let v = vec![1, 1, 2, 3, 5, 8, 13];
+        assert_eq!(v.lower_bound(&8), 5);
+        assert_eq!(v.upper_bound(&8), 6);
+        assert_eq!(v.partition_point(|x| *x < 8), 5);
+    }
 }
